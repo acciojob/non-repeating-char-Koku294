038a0264fd077cccd6325c7058557d856b093063
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-23 21:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ function firstNonRepeatedChar(str) {
 	for(let i=0;i<n;i++){
 		let isrepeated = false;
 		for(let j=0;j<n;j++){
-			if(i!==j && s[i]==s[j]){
+			if(i!==j && str[i]==str[j]){
 				isrepeated = true;
 				break;
 			}
 		}
 		if(!isrepeated){
-			return s[i];
+			return str[i];
 		}
 	}
 	
