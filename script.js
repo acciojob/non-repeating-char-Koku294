9db2fea38be68e4e0c6bd1a3c823b1c074a3c763
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-23 20:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function firstNonRepeatedChar(str) {
  // Write your code here
 	let n = str.length;
 	for(let i=0;i<n;i++){
-		if(s.indexOf(s[i])) === s.lastIndexOf(s[i]){
+		let isrepeated = false;
+		for(let j=0;j<n;j++){
+			if(i!==j && s[i]==s[j]){
+				isrepeated = true;
+				break;
+			}
+		}
+		if(!isrepeated){
 			return s[i];
 		}
 	}
+	
 	return null;
 }
 const input = prompt("Enter a string");
